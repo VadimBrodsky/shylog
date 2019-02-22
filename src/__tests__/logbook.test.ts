ee9a4c098081dfd5381bbd78a6a89af7e6e6dfb0
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-02-22 11:51:39 -0500

    Add base implementation and tests

---
+++ src/__tests__/logbook.test.ts
@@ -0,0 +1,128 @@
+import Spylog from '../lib';
+
+describe('Spylog', () => {
+  describe('constructor', () => {
+    it('should not log anything if emit set to false', () => {
+      const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => jest.fn());
+
+      const logger = new Spylog({
+        emit: false,
+        logger: console.log
+      });
+
+      const logMessage = 'Can you see this?';
+      logger.log(logMessage);
+
+      expect(consoleSpy).not.toHaveBeenCalled();
+    });
+
+    it('should accept an external logger', () => {
+      const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => jest.fn());
+
+      const logger = new Spylog({
+        emit: true,
+        logger: console.log
+      });
+
+      const logMessage = 'Can you see this?';
+      logger.log(logMessage);
+
+      expect(consoleSpy).toHaveBeenCalledWith(logMessage);
+    });
+  });
+
+  describe('.error', () => {
+    it('should store the error message', () => {
+      const logger = new Spylog();
+
+      expect(logger.error).toBeDefined();
+      logger.error('Abort', { failed: true });
+
+      // @ts-ignore
+      expect(logger.buffer).toHaveLength(1);
+    });
+  });
+
+  describe('.warn', () => {
+    it('should store the warn message', () => {
+      const logger = new Spylog();
+
+      expect(logger.warn).toBeDefined();
+      logger.warn('That', 'was', 'a', 'close', 'call');
+
+      // @ts-ignore
+      expect(logger.buffer).toHaveLength(1);
+    });
+  });
+
+  describe('.info', () => {
+    it('should store the info message', () => {
+      const logger = new Spylog();
+
+      expect(logger.info).toBeDefined();
+      logger.info('Back to normal');
+
+      // @ts-ignore
+      expect(logger.buffer).toHaveLength(1);
+    });
+  });
+
+  describe('.log', () => {
+    it('should store the log message', () => {
+      const logger = new Spylog();
+
+      expect(logger.log).toBeDefined();
+      logger.log('Something happened');
+
+      // @ts-ignore
+      expect(logger.buffer).toHaveLength(1);
+    });
+  });
+
+  describe('.setLevel', () => {
+    it('should return a custom log level function', () => {
+      const logger = new Spylog();
+      const panic = logger.setLevel('panic');
+
+      panic('FIRE! FIRE! FIRE!');
+      expect(logger.getLogs(panic)).toHaveLength(1);
+    });
+  })
+
+  describe('.getLogs', () => {
+    it('should return all logged items when has no parameters', () => {
+      const logger = new Spylog();
+
+      logger.log('Something happened');
+      logger.error('Abort', { failed: true });
+      logger.warn('That', 'was', 'a', 'close', 'call');
+      logger.info('Back to normal');
+
+      expect(logger.getLogs()).toBeDefined();
+    });
+
+    it('should return all logged  items for a specified level number', () => {
+      const logger = new Spylog();
+
+      logger.error('first error');
+      logger.error('second error');
+      logger.info('something something');
+      const logs = logger.getLogs(1);
+
+      expect(logs).toBeDefined();
+      expect(logs).toHaveLength(2);
+    });
+
+    it('should return all logged items for a specified log function', () => {
+      const logger = new Spylog();
+
+      logger.warn('warn 1st');
+      logger.warn('warn 2nd');
+      logger.info('something something');
+      const logs = logger.getLogs(logger.warn);
+
+      expect(logs).toBeDefined();
+      expect(logs).toHaveLength(2);
+    });
+  });
+});
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,65 @@
-class Logbook {
-  private emit: boolean;
+interface Log {
+  level: LogLevel | string;
+  time: number;
+  msg: string;
+}
 
-  constructor() {
-    this.emit = false;
+const enum LogLevel {
+  error = 1,
+  warn = 2,
+  info = 3,
+  log = 4,
+  // debug = 5,
+  // trace = 6,
+}
+
+interface LogFn {
+  (...args: unknown[]): void;
+  level: Log['level'];
+}
+
+class Spylog {
+  private emit: boolean;
+  private buffer: Log[];
+  private externalLogger: (...args: unknown[]) => unknown;
+  private customLevel: {[key: string]: LogFn}
+
+  public error: LogFn;
+  public warn: LogFn;
+  public info: LogFn;
+  public log: LogFn;
+
+  constructor({ emit, logger } = { emit: false, logger: console.log }) {
+    this.emit = emit;
+    this.externalLogger = logger;
+    this.buffer = [];
+    this.customLevel = {};
+
+    this.error = this.setLevel(1);
+    this.warn = this.setLevel(2);
+    this.info = this.setLevel(3);
+    this.log = this.setLevel(4);
   }
 
-  public log(...args: unknown[]) {
-    this.emit && console.log(...args);
+  public getLogs(level?: Log['level'] | LogFn) {
+    if (typeof level === 'function') {
+      return this.buffer.filter((log) => log.level === level.level);
+    } else {
+      return level ? this.buffer.filter((log) => log.level === level) : this.buffer;
+    }
+  }
+
+  public setLevel(level: Log['level'] | string): LogFn {
+    const logFn: LogFn = (...args: unknown[]) => {
+      this.buffer.push({ level, time: Date.now(), msg: args.join(' ') });
+      this.emit && this.externalLogger(...args);
+    };
+
+    logFn.level = level;
+    this.customLevel[level] = logFn;
+
+    return logFn;
   }
 }
+
+export default Spylog;
